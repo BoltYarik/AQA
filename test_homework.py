61ef8775ff69ba66731810108fcbee482204c29a
Yaroslav Baltak <72920114+BoltYarik@users.noreply.github.com>
2023-12-01 18:12:51 +0200

    Update test_homework.py

--- test_homework.py
+++ test_homework.py
@@ -3,25 +3,23 @@ import pytest
 
 session = requests.session()
 
-def register_user(name, LastName, email, password, repeatPassword):
+def register_user(name, lastName, email, password, repeatPassword):
     url = "https://qauto2.forstudy.space/api/auth/signup"
     data = {
         "name": name,
-        "LastName": LastName,
+        "lastName": lastName,
         "email": email,
         "password": password,
         "repeatPassword": repeatPassword
     }
-    response = session.post(url=url, json=data
-    return print(response)
+    response = session.post(url=url, json=data)
+    return response
 
-
-register_user("Zaraki", "Kenpache", "testho4mewok11@gmail.com", "Qwerty12345", "Qwerty12345")
 #Параметризований тест для реєстрації юзера
 @pytest.mark.parametrize("name,LastName,email,password,repeatPassword, expected_status_code",
                         [
-                            ("Zaraki", "Kenpache", "testho4mewok11@gmail.com", "Qwerty12345", "Qwerty12345", 201),
-                            ("Vol", "Wat", "testhomework22@gmail.com", "Qwerty12345", "Qwerty12345", 201),
+                            ("Zaraki", "Kenpache", "testhoy4merrrwok11@gmail.com", "Qwerty12345", "Qwerty12345", 201),
+                            ("Vol", "Wat", "testhomewrrefhhork22@gmail.com", "Qwerty12345", "Qwerty12345", 201),
                             ("l", "password456", "testhomework33@gmail.com", "33", "33", 400),
                         ])
 def test_user_registration(name, LastName, email, password, repeatPassword, expected_status_code):
